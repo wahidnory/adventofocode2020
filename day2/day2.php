632c031f655c8c99216e8a82381a8cac3b975602
Wahid <wahid_kl@hotmail.com>
2020-12-04 21:46:16 +0100

    day 2 - in progress

--- day2/day2.php
+++ day2/day2.php
@@ -1,55 +1,27 @@
-<?php 
-class DayOne {
+<?php
+// loop through the text lines
+// filter the lines according to:
+// numbers are minimum and max amount of letters required in the password;
+// letter indicates the letter itself.
+class DayTwo {
 	private $file = './input.txt';
 	private $fileLines = [];
-	private $index = 0;
-	private $found2020 = false;
-	private $base = 0;
-	private $match = 0;
-	private $anotherMatch = 0;
 
 	/**
-	 *	Lets setup basics and boot it.
+	 * Setup file reading and boot parsing 
 	 */
 	public function __construct() {
 		$this->fileLines = file($this->file);
-		$this->find2020();
 	}
 
-	/**
-	 *	Find 2020.
-	 */ 
-	public function find2020() {
-		foreach($this->fileLines as $base) {
-			foreach($this->fileLines as $possibleMatch) {
-				foreach($this->fileLines as $anotherPossibleMatch) {
-					$this->base = (int) $base;
-					$this->match = (int) $possibleMatch;
-					$this->anotherMatch = (int) $anotherPossibleMatch; 
-					
-					$sum =  $this->base + $this->match + $this->anotherMatch;
-					
-					if($sum === 2020) {
-						break 3;
-					}
-				}
-			}
-		}		
+	public function findPasswords() {
 
-		// we found 2020 
-		return $this->printResult();
 	}
 
-	/**
-	 * Just print something so we know what's up.
-	 */
 	public function printResult() {
-		echo "match :: " . $this->match ."\r\n"; 
-		echo "match :: " . $this->base ."\r\n";
-		echo "match :: " . $this->anotherMatch . "\r\n";
-
-		echo "Result :: " . $this->match * $this->base * $this->anotherMatch;
+		echo "Amount of password ::" . $this->passwordMatch;
 	}
 }
 
-new DayOne();
+
+new DayTwo();
