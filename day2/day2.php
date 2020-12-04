2d24e5069d7271e9f768ed2d7b9c6282b1a82da9
Wahid <wahid_kl@hotmail.com>
2020-12-05 00:28:47 +0100

    Day 2 - Part 1 - Attempt 1.

--- day2/day2.php
+++ day2/day2.php
@@ -6,20 +6,38 @@
 class DayTwo {
 	private $file = './input.txt';
 	private $fileLines = [];
+	private $matches = [];
 
 	/**
 	 * Setup file reading and boot parsing 
 	 */
 	public function __construct() {
 		$this->fileLines = file($this->file);
+		$this->findPasswords();
 	}
 
+	/**
+	 * Find the passwords that match in the list
+	 */
 	public function findPasswords() {
+		foreach($this->fileLines as $line) {
+			$policy = explode(':', $line);
+			$password = explode(': ', $line);
+			$letterToMatch = explode(' ', $policy[0]);	
+			$minMax = explode('-', $letterToMatch[0]);
+			// perform the search
+			$foundAmount = substr_count($password[1], $letterToMatch[1]);
 
+			if($foundAmount >= $minMax[0] && $foundAmount <= $minMax[1]) {
+				$this->matches[] = [$foundAmount, $password[1], $letterToMatch[1], $minMax];
+			}
+		}
+
+		$this->printResult();
 	}
 
 	public function printResult() {
-		echo "Amount of password ::" . $this->passwordMatch;
+		echo "Amount of password ::" . count($this->matches);
 	}
 }
 
